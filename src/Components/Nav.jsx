7bfcaf35e758ec46238df38e592a76ea85a0fb51
brian-ambani <brainambani1@gmail.com>
2023-09-21 13:39:33 +0300

    updated the nav

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className='container w-3/4 mx-auto text-bold flex items-center justify-between py-10 text-xl font-bold'>
+    <div className='container w-3/4 mx-auto text-bold flex items-center justify-between py-10 text-xl font-bold font-mono'>
         <ul className='flex space-x-8'>
-            <a href=""><li>FHub</li></a>
-            <a href=""><li>Watch</li></a>
-            <a href=""><li>Explore</li></a>
-            <a href=""><li>Get</li></a>
-            <a href=""><li>Pay</li></a>
-            <a href=""><li>MyFHub</li></a>
-            <a href=""><li>About</li></a>
+            <a className="hover:text-slate-400" href=""><li>FHub</li></a>
+            <a className="hover:text-slate-400" href=""><li>Watch</li></a>
+            <a className="hover:text-slate-400" href=""><li>Explore</li></a>
+            <a className="hover:text-slate-400" href=""><li>Get</li></a>
+            <a className="hover:text-slate-400" href=""><li>Pay</li></a>
+            <a className="hover:text-slate-400" href=""><li>MyFHub</li></a>
+            <a className="hover:text-slate-400" href=""><li>About</li></a>
         </ul >
         <ul className='space-x-12 flex'>
-          <a href=""><li>Search</li></a>
-          <a href=""><li>Sign in</li></a>
+          <a className="hover:text-slate-400" href=""><li>Search</li></a>
+          <a className="hover:text-slate-400" href=""><li>Sign in</li></a>
         </ul>
     </div>
   )
